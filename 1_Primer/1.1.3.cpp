655e5e4228691de406c823576398b8013890d606
Stefan Ajax <pianoajax@gmail.com>
2019-12-28 21:36:15 +0100

    asdasd

--- 1_Primer/1.1.3.cpp
+++ 1_Primer/1.1.3.cpp
@@ -4,15 +4,10 @@
 int main(){
     char ch = 'Q';
     void* np = &ch;
-    //char* p = &ch;
-    //std::cout << *p;
-    //std::cout << std::endl;
-    //*p = 'Z';
     std::cout << np;
     std::cout << std::endl;
     int test[5];
     for (int i = 0; i<10000; i++){
         std::cout << test[i] << std::endl;       
-    }
-    
+    }    
 }
